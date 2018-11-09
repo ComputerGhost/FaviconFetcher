a79585071b48b7fd3d94422b6bf3fe46f9b22bad
computerghost <nathan.belue@gmail.com>
2018-11-08 19:49:36 -0800

    Added support for ImageResource

--- FaviconFetcher.Tests/MockSource.cs
+++ FaviconFetcher.Tests/MockSource.cs
@@ -8,25 +8,53 @@ using System.Threading.Tasks;
 
 namespace FaviconFetcher.Tests
 {
-    class MockSource : ISource
+    class MockSource : ISource, IDisposable
     {
-        private Dictionary<Uri, string> _resourceMap = new Dictionary<Uri, string>();
+        private Dictionary<Uri, string> _textResourceMap = new Dictionary<Uri, string>();
+        private Dictionary<Uri, List<Image>> _imageResourceMap = new Dictionary<Uri, List<Image>>();
+
+        public void Dispose()
+        {
+            foreach (var imageResource in _imageResourceMap)
+            {
+                foreach (var image in imageResource.Value)
+                    image.Dispose();
+            }
+        }
 
         public void AddTextResource(Uri uri, string contents)
         {
-            _resourceMap.Add(uri, contents);
+            _textResourceMap.Add(uri, contents);
+        }
+
+        public void AddImageResource(Uri uri, Size imageSize)
+        {
+            using (var bitmap = new Bitmap(1, 1))
+            {
+                bitmap.SetPixel(0, 0, Color.DarkSlateBlue);
+                AddImageResource(uri, new Bitmap(bitmap, imageSize));
+            }
+        }
+
+        public void AddImageResource(Uri uri, Image image)
+        {
+            if (!_imageResourceMap.ContainsKey(uri))
+                _imageResourceMap.Add(uri, new List<Image>());
+            _imageResourceMap[uri].Add(image);
         }
 
         public IEnumerable<Image> DownloadImages(Uri uri)
         {
-            throw new NotImplementedException();
+            if (!_imageResourceMap.ContainsKey(uri))
+                return null;
+            return _imageResourceMap[uri];
         }
 
         public StreamReader DownloadText(Uri uri)
         {
-            if (!_resourceMap.ContainsKey(uri))
+            if (!_textResourceMap.ContainsKey(uri))
                 return null;
-            var contents = _resourceMap[uri];
+            var contents = _textResourceMap[uri];
 
             var memoryStream = new MemoryStream();
             var writer = new StreamWriter(memoryStream);
@@ -35,5 +63,6 @@ namespace FaviconFetcher.Tests
             memoryStream.Position = 0;
             return new StreamReader(memoryStream);
         }
+
     }
 }
