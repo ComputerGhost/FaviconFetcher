aaf3398c6550c2d622a7ace32855a186a5dfacae
computerghost <nathan.belue@gmail.com>
2018-11-08 20:01:18 -0800

    Added Fetcher unit tests.

---
+++ FaviconFetcher.Tests/FetcherTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FaviconFetcher.Tests
+{
+    [TestClass]
+    public class FetcherTests
+    {
+        [TestMethod]
+        public void ShouldMakeTwoRequestsWhenNoneSpecified()
+        {
+            var uri = new Uri("http://www.example.com");
+            var source = new MockSource();
+            source.AddTextResource(uri, "Fake content.");
+
+            var fetcher = new Fetcher(source);
+            fetcher.FetchClosest(uri, new Size(16, 16));
+
+            Assert.AreEqual(source.RequestCount, 2);
+        }
+    }
+}
